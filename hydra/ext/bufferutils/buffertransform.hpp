91d4ed16152f9748a9e6f5bb6fc46a2912110a53
akin <akin@localhost>
2013-02-28 23:41:22 +0200

    this is demonic work.. but it has to be done.. Once its done its DONE.

--- hydra/ext/bufferutils/buffertransform.hpp
+++ hydra/ext/bufferutils/buffertransform.hpp
@@ -14,6 +14,9 @@
 namespace buffer {
 namespace utils {
 
+/////////
+// RGB8
+/////////
 template <typename TargetType>
 bool transformRGB8toRGBA( const unsigned char *source , TargetType *target , size_t pixelcount )
 {
@@ -64,14 +67,14 @@ bool transformRGB8toARGB( const unsigned char *source , TargetType *target , siz
 	}
 	return true;
 }
-bool transformRGB8toRGB565( const unsigned char *source , unsigned short *target , size_t pixelcount )
+bool transformRGBA8toRGB565( const unsigned char *source , unsigned short *target , int mul , size_t pixelcount )
 {
 	for( size_t i = 0 ; i < pixelcount ; ++i )
 	{
 		target[ i ] =
-				(((unsigned int)source[ i * 3 + 0 ] << 8 ) & 0xF800 ) |
-				(((unsigned int)source[ i * 3 + 1 ] << 3 ) & 0x7E0 ) |
-				(((unsigned int)source[ i * 3 + 2 ] >> 3 ) & 0x1F );
+				(((unsigned int)source[ i * mul + 0 ] << 8 ) & 0xF800 ) |
+				(((unsigned int)source[ i * mul + 1 ] << 3 ) & 0x7E0 ) |
+				(((unsigned int)source[ i * mul + 2 ] >> 3 ) & 0x1F );
 	}
 	return true;
 }
@@ -95,20 +98,13 @@ bool transformRGB8toRGBA5551( const unsigned char *source , unsigned short *targ
 				(((unsigned int)source[ i * 3 + 0 ] << 8 ) & 0xF800 ) |
 				(((unsigned int)source[ i * 3 + 1 ] << 3 ) & 0x7E0 ) |
 				(((unsigned int)source[ i * 3 + 2 ] >> 2 ) & 0x1F ) |
-				(((unsigned int)source[ i * 3 + 2 ] >> 7 ) & 0x1 );
+				0x1;
 	}
 	return true;
 }
 bool transformRGB8toRGBA12( const unsigned char *source , unsigned char *target , size_t pixelcount )
 {
 	// TODO
-	for( size_t i = 0 ; i < pixelcount ; ++i )
-	{
-		target[ i * 4 + 0 ] = 0xFF;
-		target[ i * 4 + 1 ] = source[ i * 3 + 0 ];
-		target[ i * 4 + 2 ] = source[ i * 3 + 1 ];
-		target[ i * 4 + 3 ] = source[ i * 3 + 2 ];
-	}
 	return false;
 }
 bool transformRGB8(
@@ -137,7 +133,7 @@ bool transformRGB8(
 		}
 		case pixel::RGB565 :
 		{
-			return transformRGB8toRGB565( source , (unsigned short *)target , pixelcount );
+			return transformRGBA8toRGB565( source , (unsigned short *)target , 3 , pixelcount );
 		}
 		case pixel::RGBA4 :
 		{
@@ -160,6 +156,334 @@ bool transformRGB8(
 			break;
 		}
 	}
+	return false;
+}
+
+/////////
+// RGBA8
+/////////
+template <typename TargetType>
+bool transformRGBA8toRGBA( const unsigned char *source , TargetType *target , size_t pixelcount )
+{
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		target[ i * 4 + 0 ] = source[ i * 4 + 0 ];
+		target[ i * 4 + 1 ] = source[ i * 4 + 1 ];
+		target[ i * 4 + 2 ] = source[ i * 4 + 2 ];
+		target[ i * 4 + 3 ] = source[ i * 4 + 3 ];
+	}
+	return true;
+}
+// float specialization
+template <>
+bool transformRGBA8toRGBA<float>( const unsigned char *source , float *target , size_t pixelcount )
+{
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		target[ i * 4 + 0 ] = source[ i * 4 + 0 ] / 256.0f;
+		target[ i * 4 + 1 ] = source[ i * 4 + 1 ] / 256.0f;
+		target[ i * 4 + 2 ] = source[ i * 4 + 2 ] / 256.0f;
+		target[ i * 4 + 3 ] = source[ i * 4 + 3 ] / 256.0f;
+	}
+	return true;
+}
+// double specialization
+template <>
+bool transformRGBA8toRGBA<double>( const unsigned char *source , double *target , size_t pixelcount )
+{
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		target[ i * 4 + 0 ] = source[ i * 4 + 0 ] / 256.0;
+		target[ i * 4 + 1 ] = source[ i * 4 + 1 ] / 256.0;
+		target[ i * 4 + 2 ] = source[ i * 4 + 2 ] / 256.0;
+		target[ i * 4 + 3 ] = source[ i * 4 + 3 ] / 256.0;
+	}
+	return true;
+}
+template <typename TargetType>
+bool transformRGBA8toRGB( const unsigned char *source , TargetType *target , size_t pixelcount )
+{
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		target[ i * 3 + 0 ] = source[ i * 4 + 0 ];
+		target[ i * 3 + 1 ] = source[ i * 4 + 1 ];
+		target[ i * 3 + 2 ] = source[ i * 4 + 2 ];
+	}
+	return true;
+}
+template <typename TargetType>
+bool transformRGBA8toARGB( const unsigned char *source , TargetType *target , size_t pixelcount )
+{
+	unsigned char alpha;
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		alpha = source[ i * 4 + 3 ];
+
+		target[ i * 4 + 3 ] = source[ i * 4 + 2 ];
+		target[ i * 4 + 2 ] = source[ i * 4 + 1 ];
+		target[ i * 4 + 1 ] = source[ i * 4 + 0 ];
+		target[ i * 4 + 0 ] = alpha;
+	}
+	return true;
+}
+bool transformRGBA8toRGBA4( const unsigned char *source , unsigned short *target , size_t pixelcount )
+{
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		target[ i ] =
+				(((unsigned int)source[ i * 4 + 0 ] << 8 ) & 0xF000 ) |
+				(((unsigned int)source[ i * 4 + 1 ] << 4 ) & 0x0F00 ) |
+				(((unsigned int)source[ i * 4 + 2 ] )      & 0x00F0 ) |
+				(((unsigned int)source[ i * 4 + 3 ] >> 4 ) & 0x000F );
+	}
+	return true;
+}
+bool transformRGBA8toRGBA5551( const unsigned char *source , unsigned short *target , size_t pixelcount )
+{
+	for( size_t i = 0 ; i < pixelcount ; ++i )
+	{
+		target[ i ] =
+				(((unsigned int)source[ i * 4 + 0 ] << 8 ) & 0xF800 ) |
+				(((unsigned int)source[ i * 4 + 1 ] << 3 ) & 0x7E0 ) |
+				(((unsigned int)source[ i * 4 + 2 ] >> 2 ) & 0x1F ) |
+				(((unsigned int)source[ i * 4 + 3 ] >> 7 ) & 0x1 );
+	}
+	return true;
+}
+bool transformRGBA8toRGBA12( const unsigned char *source , unsigned char *target , size_t pixelcount )
+{
+	// TODO
+	return false;
+}
+bool transformRGBA8(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	switch( tformat )
+	{
+		case pixel::RGB8 :
+		{
+			return transformRGBA8toRGB( source , (unsigned char *)target , pixelcount );
+		}
+		case pixel::ARGB8 :
+		{
+			return transformRGBA8toARGB( source , (unsigned char *)target , pixelcount );
+		}
+		case pixel::RGBA16 :
+		{
+			return transformRGBA8toRGBA( source , (unsigned short *)target , pixelcount );
+		}
+		case pixel::RGBA32 :
+		{
+			return transformRGBA8toRGBA( source , (unsigned int *)target , pixelcount );
+		}
+		case pixel::RGB565 :
+		{
+			return transformRGBA8toRGB565( source , (unsigned short *)target , 4 , pixelcount );
+		}
+		case pixel::RGBA4 :
+		{
+			return transformRGBA8toRGBA4( source , (unsigned short *)target , pixelcount );
+		}
+		case pixel::RGBA5551 :
+		{
+			return transformRGBA8toRGBA5551( source , (unsigned short *)target , pixelcount );
+		}
+		case pixel::RGBA32F :
+		{
+			return transformRGBA8toRGBA( source , (float *)target , pixelcount );
+		}
+		case pixel::RGBA12 :
+		{
+			return transformRGBA8toRGBA12( source , (unsigned char *)target , pixelcount );
+		}
+		default:
+		{
+			break;
+		}
+	}
+	return false;
+}
+
+/////////
+// ARGB8
+/////////
+bool transformARGB8(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// ALPHA8
+/////////
+bool transformALPHA8(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// LUMINANCE
+/////////
+bool transformLUMINANCE(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// INTENSITY
+/////////
+bool transformINTENSITY(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGBA12
+/////////
+bool transformRGBA12(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGBA16
+/////////
+bool transformRGBA16(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGBA32
+/////////
+bool transformRGBA32(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGBA32F
+/////////
+bool transformRGBA32F(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// DEPTH8
+/////////
+bool transformDEPTH8(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// DEPTH16
+/////////
+bool transformDEPTH16(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// DEPTH24
+/////////
+bool transformDEPTH24(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// DEPTH32
+/////////
+bool transformDEPTH32(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGB565
+/////////
+bool transformRGB565(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGBA4
+/////////
+bool transformRGBA4(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
+	return false;
+}
+
+/////////
+// RGBA5551
+/////////
+bool transformRGBA5551(
+		const unsigned char *source ,
+		void *target ,
+		pixel::Format tformat ,
+		size_t pixelcount )
+{
 	return false;
 }
 
--- hydra/ext/bufferutils/bufferutils.cpp
+++ hydra/ext/bufferutils/bufferutils.cpp
@@ -22,28 +22,6 @@ bool transformColor(
 		pixel::Format tformat ,
 		const glm::ivec2& dimensions )
 {
-	/*
-	 * pixel::Format :
-	NONE = 0,
-	RGB8 = 1,
-	RGBA8 = 2,
-	ARGB8 = 3,
-	ALPHA8 = 4,
-	LUMINANCE = 5,
-	INTENSITY = 6,
-	RGBA12 = 7,
-	RGBA16 = 8,
-	RGBA32 = 9,
-	RGBA32F = 10,
-	DEPTH8 = 11,
-	DEPTH16 = 12,
-	DEPTH24 = 13,
-	DEPTH32 = 14,
-	RGB565 = 15,
-	RGBA4 = 16,
-	RGBA5551 = 17,
-	*/
-
 	size_t pixelcount = dimensions.x * dimensions.y;
 	size_t sbypp = pixel::getByteSize( sformat );
 	size_t tbypp = pixel::getByteSize( tformat );
@@ -73,7 +51,67 @@ bool transformColor(
 		}
 		case pixel::RGBA8 :
 		{
-			break;
+			return transformRGBA8( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::ARGB8 :
+		{
+			return transformARGB8( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::ALPHA8 :
+		{
+			return transformALPHA8( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::LUMINANCE :
+		{
+			return transformLUMINANCE( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::INTENSITY :
+		{
+			return transformINTENSITY( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGBA12 :
+		{
+			return transformRGBA12( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGBA16 :
+		{
+			return transformRGBA16( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGBA32 :
+		{
+			return transformRGBA32( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGBA32F :
+		{
+			return transformRGBA32F( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::DEPTH8 :
+		{
+			return transformDEPTH8( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::DEPTH16 :
+		{
+			return transformDEPTH16( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::DEPTH24 :
+		{
+			return transformDEPTH24( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::DEPTH32 :
+		{
+			return transformDEPTH32( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGB565 :
+		{
+			return transformRGB565( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGBA4 :
+		{
+			return transformRGBA4( (const unsigned char*)source , target , tformat , pixelcount );
+		}
+		case pixel::RGBA5551 :
+		{
+			return transformRGBA5551( (const unsigned char*)source , target , tformat , pixelcount );
 		}
 		default:
 		{
